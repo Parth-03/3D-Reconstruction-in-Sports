8f838dba41a0350fa22a3b0b9fe08d03f66dbd68
Yuni Jeong <seoyunjeong@umass.edu>
2024-05-16 00:42:12 -0400

    added dummy trans_to_inter and trans_iter_t

--- main/size_depth_optimization/size_depth_optimization.py
+++ main/size_depth_optimization/size_depth_optimization.py
@@ -272,6 +272,8 @@ def joint_opt_use_smpl_for_reproj(
     # We could just start from a random tensor, not from trans_to_inter and trans_iter_t?
     # trans_to_inter[:, None, :]: numpy array with shape (3,1,3)
     # trans_iter_t: torch tensor with shape [3,1,1]
+    trans_to_inter = np.random.rand(3, 1, 3)
+    trans_iter_t = torch.randn(3, 1, 1)
     opt_trans = torch.tensor(
         trans_to_inter[:, None, :],
         requires_grad=True,
